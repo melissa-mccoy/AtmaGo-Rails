ac61647bc20cfbba46ed734fc535cee825361ed2
Melissa McCoy <apprentice@master-inf-4.local>
2014-08-28 16:54:00 -0700

    adding migration

---
+++ db/migrate/20140828234851_create_vendors.rb
@@ -0,0 +1,16 @@
+class CreateVendors < ActiveRecord::Migration
+  def change
+    create_table :vendors do |t|
+      t.string :name
+      t.string :country
+      t.string :state
+      t.string :city
+      t.string :postalcode
+      t.string :telephone
+      t.decimal :price
+      t.text :note
+      t.float :latitude
+      t.float :longitude
+    end
+  end
+end
